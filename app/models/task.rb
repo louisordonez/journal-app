c13756e79e3e73467971a2353fe8b280b5693799
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-09-26 23:34:30 +0800

    Add validation for tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { minimum: 2 }
+  validates :deadline, presence: true
 end
