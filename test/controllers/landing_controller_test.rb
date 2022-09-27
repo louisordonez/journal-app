63f85e4ec677a30904911616f4ae58194e6a771c
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-09-27 21:40:42 +0800

    Refactor landing controller test

--- test/controllers/landing_controller_test.rb
+++ test/controllers/landing_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class LandingControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    sign_in users(:user1)
-    sign_out users(:user1)
-  end
-
   test 'should get index' do
     get root_path
     assert_response :success
